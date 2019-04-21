ded3fcd7c0a144e0a552d8562d1527fdcbaf477d
William Sanders <william.sanders076@gmail.com>
2019-04-20 22:31:47 -0400

    Scaffold F sharp solution to UtopianTree

---
+++ UtopianTree/FSharp/UtopianTree/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    let strNumber = Console.ReadLine();
+    let (_, number) = Int32.TryParse strNumber
+
+    Console.WriteLine(square number)
+    Console.ReadKey();
+    0 // return an integer exit code
