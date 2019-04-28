a3af82d5bb3ada33a093ef2298deef95739b1832
William Sanders <william.sanders076@gmail.com>
2019-04-28 13:09:16 -0400

    Improve formatting

--- AngryProfessor/FSharp/AngryProfessor/Program.fs
+++ AngryProfessor/FSharp/AngryProfessor/Program.fs
@@ -7,8 +7,12 @@ type ProfessorClass = {
 }
 
 let getProfessorClass() = 
-    let studentsThreshold = stdin.ReadLine().Split(' ') |> Array.map (fun i -> i |> int)
-    let arrivalTimes = stdin.ReadLine().Split(' ') |> Array.map (fun i -> i |> int)
+    let studentsThreshold = 
+        stdin.ReadLine().Split(' ') 
+        |> Array.map (fun i -> i |> int)
+    let arrivalTimes = 
+        stdin.ReadLine().Split(' ') 
+        |> Array.map (fun i -> i |> int)
     {
         students = studentsThreshold.[0]; 
         threshold = studentsThreshold.[1]; 
@@ -17,19 +21,29 @@ let getProfessorClass() =
 
 let getProfessorClasses cases =
     let rec f case classes = 
-        if case = 0 then classes else f (case - 1) (getProfessorClass() :: classes)
+        if case = 0 
+        then classes 
+        else f (case - 1) (getProfessorClass() :: classes)
     f cases []
 
 let lateStudents arrivalTimes = 
-    arrivalTimes |> Array.filter (fun i -> i > 0) |> Array.length
+    arrivalTimes 
+        |> Array.filter (fun i -> i > 0) 
+        |> Array.length
 
 let classCanceled professorClass = 
     let lateStudents = lateStudents professorClass.arrivalTimes
-    if professorClass.students - lateStudents < professorClass.threshold then printf "YES\n" else printf "NO\n"
+    if professorClass.students - lateStudents < professorClass.threshold 
+    then printf "YES\n" 
+    else printf "NO\n"
 
 [<EntryPoint>]
 let main _ =
     let cases = Console.ReadLine() |> int
-    let classes = getProfessorClasses cases |> List.rev
-    classes |> List.map (fun c -> classCanceled c) |> ignore
+    let classes = 
+        getProfessorClasses cases 
+        |> List.rev
+    classes 
+        |> List.map (fun c -> classCanceled c) 
+        |> ignore
     0
