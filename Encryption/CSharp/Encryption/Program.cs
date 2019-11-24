162339ea5d30c0a31d5dffdd32a47a063742ad36
William Sanders <william.sanders076@gmail.com>
2019-11-23 19:25:37 -0500

    Add C# solution for Encryption

---
+++ Encryption/CSharp/Encryption/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Solution
+{
+
+    static string Encryption(string s)
+    {
+        var spacelessString = s.Replace(" ", string.Empty);
+        var spacelessStringLength = spacelessString.Length;
+        var squareRoot = Math.Sqrt(spacelessStringLength);
+
+        var rows = (int)Math.Floor(squareRoot);
+        var columns = (int)Math.Ceiling(squareRoot);
+
+        var sb = new StringBuilder();
+        for (var row = 0; row <= rows; row++)
+        {
+            for (var column = row; column < spacelessStringLength; column += columns)
+            {
+                if (column < spacelessStringLength && sb.Length < spacelessStringLength + rows)
+                {
+                    sb.Append(spacelessString[column]);
+                }
+            }
+
+            sb.Append(" ");
+        }
+
+        return sb.ToString();
+    }
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        string result = Encryption(s);
+        Console.WriteLine(result);
+    }
+}
