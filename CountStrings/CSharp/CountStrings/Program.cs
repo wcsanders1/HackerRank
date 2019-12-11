5fc522a0ec77fee0bf96cd8827a739119440ef0a
William Sanders <william.sanders076@gmail.com>
2019-12-10 20:29:54 -0500

    Scaffold C# solution to CountStrings

---
+++ CountStrings/CSharp/CountStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CountStrings
+{
+    public enum RegularExpressionElement
+    {
+        A,
+        B,
+        Astrisk
+    }
+
+    public static class RegularExpressionUtility
+    {
+        public static RegularExpressionElement ConvertCharToRegularExpressionElement(char character)
+        {
+            return character switch
+            {
+                'a' => RegularExpressionElement.A,
+                'b' => RegularExpressionElement.B,
+                _ => RegularExpressionElement.Astrisk,
+            };
+        }
+    }
+
+    class RegularExpression
+    {
+        public RegularExpressionElement FirstElement { get; set; }
+        public RegularExpressionElement SecondElement { get; set; }
+        public RegularExpression NestedRegularExpression { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
