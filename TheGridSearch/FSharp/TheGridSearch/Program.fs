30476d5f892373ed9b27cf0092864bc2074052b1
William Sanders <william.sanders.dev@gmail.com>
2020-01-11 12:01:33 -0500

    Scaffold FSharp solution to The Grid Search

---
+++ TheGridSearch/FSharp/TheGridSearch/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "This doesn't do anything yet, except print this, which is something, I guess."
+    0 // return an integer exit code
