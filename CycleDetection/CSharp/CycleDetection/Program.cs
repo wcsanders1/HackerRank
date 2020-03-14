7beb7cf1bf88e20e81008f02f16e581c299b1187
William Sanders <william.sanders076@gmail.com>
2020-03-14 10:38:23 -0400

    CycleDetection CSharp solution

---
+++ CycleDetection/CSharp/CycleDetection/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    static bool hasCycle(SinglyLinkedListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return false;
+        }
+
+        var nodeValues = new HashSet<SinglyLinkedListNode>
+        {
+            head
+        };
+
+        var nextNode = head.next;
+
+        while(nextNode != null)
+        {
+            if (nodeValues.Contains(nextNode))
+            {
+                return true;
+            }
+
+            nodeValues.Add(nextNode);
+            nextNode = nextNode.next;
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        
+    }
+}
