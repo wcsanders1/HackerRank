a557324844581e2bf2cf287bf4af69b3abff94e0
William Sanders <william.sanders076@gmail.com>
2020-03-29 18:06:01 -0400

    CSharp solution to FibonacciModified

---
+++ FibonacciModified/CSharp/FibonacciModified/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+
+namespace FibonacciModified
+{
+    class Program
+    {
+        static BigInteger FibonacciModified(int t1, int t2, int n)
+        {
+            if (n == 1)
+            {
+                return t1;
+            }
+
+            if (n == 2)
+            {
+                return t2;
+            }
+
+            var newT1 = new BigInteger(t1);
+            var newT2 = new BigInteger(t2);
+
+            while (n > 2)
+            {
+                n--;
+                var oldT2 = newT2;
+                newT2 = newT1 + newT2 * newT2;
+                newT1 = oldT2;
+            }
+
+            return newT2;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var t1 = int.Parse(input[0]);
+            var t2 = int.Parse(input[1]);
+            var n = int.Parse(input[2]);
+
+            var result = FibonacciModified(t1, t2, n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
