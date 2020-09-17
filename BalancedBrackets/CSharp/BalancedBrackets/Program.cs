72324427b94e383633f723d78ff10ca51159a9f9
William Sanders <william.sanders076@gmail.com>
2020-09-17 06:19:05 -0400

    CSharp solution to BalancedBrackets

---
+++ BalancedBrackets/CSharp/BalancedBrackets/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+
+class Solution
+{
+    static string IsBalanced(string s)
+    {
+        var bracketStack = new Stack<char>();
+
+        foreach (var bracket in s)
+        {
+            if (bracket == '(' || bracket == '[' || bracket == '{')
+            {
+                bracketStack.Push(bracket);
+            }
+            else
+            {
+                if (bracketStack.Count == 0)
+                {
+                    return "NO";
+                }
+
+                var openBracket = bracketStack.Pop();
+                if ((openBracket == '(' && bracket != ')') ||
+                    (openBracket == '[' && bracket != ']') ||
+                    (openBracket == '{' && bracket != '}'))
+                {
+                    return "NO";
+                }
+            }
+        }
+
+        if (bracketStack.Count > 0)
+        {
+            return "NO";
+        }
+
+        return "YES";
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = IsBalanced(s);
+
+            Console.WriteLine(result);
+        }
+
+        Console.ReadKey();
+    }
+}
