998637b84bcfce21d9765580cd366ad92ef97857
William Sanders <william.sanders076@gmail.com>
2020-10-25 19:39:52 -0400

    Scaffold Python solution to EvenTree

---
+++ EvenTree/Python/EventTree.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the evenForest function below.
+
+
+def evenForest(t_nodes, t_edges, t_from, t_to):
+    return 0
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t_nodes, t_edges = map(int, input().rstrip().split())
+
+    t_from = [0] * t_edges
+    t_to = [0] * t_edges
+
+    for i in range(t_edges):
+        t_from[i], t_to[i] = map(int, input().rstrip().split())
+
+    res = evenForest(t_nodes, t_edges, t_from, t_to)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
