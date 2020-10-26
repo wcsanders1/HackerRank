ef6523035e0816a7d985841a893f02fdefeaadc8
William Sanders <william.sanders076@gmail.com>
2020-10-26 06:38:23 -0400

    Create tree in EvenTree Python solution

--- EvenTree/Python/EventTree.py
+++ EvenTree/Python/EventTree.py
@@ -6,16 +6,37 @@ import random
 import re
 import sys
 
-# Complete the evenForest function below.
+
+class Node:
+    value = 0
+    total_children = 0
+    children = []
+
+    def __init__(self, value):
+        self.value = value
 
 
-def evenForest(t_nodes, t_edges, t_from, t_to):
+def get_or_create_node(value, nodes_map):
+    if value not in nodes_map:
+        new_node = Node(value)
+        nodes_map[value] = new_node
+    return nodes_map[value]
+
+
+def even_forest(t_nodes, t_edges, t_from, t_to):
+    root = Node(1)
+    nodes_map = {1: root}
+
+    for i in range(t_from):
+        child = get_or_create_node(t_from[i], nodes_map)
+        parent = get_or_create_node(t_to[i], nodes_map)
+
+        parent.children.append(child)
+
     return 0
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     t_nodes, t_edges = map(int, input().rstrip().split())
 
     t_from = [0] * t_edges
@@ -24,8 +45,6 @@ if __name__ == '__main__':
     for i in range(t_edges):
         t_from[i], t_to[i] = map(int, input().rstrip().split())
 
-    res = evenForest(t_nodes, t_edges, t_from, t_to)
+    res = even_forest(t_nodes, t_edges, t_from, t_to)
 
-    fptr.write(str(res) + '\n')
-
-    fptr.close()
+    print(str(res) + '\n')
