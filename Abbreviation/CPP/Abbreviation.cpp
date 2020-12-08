a14e8798b344761c0357bcbe95448fa9ba64e2b8
William Sanders <william.sanders076@gmail.com>
2020-12-08 06:50:56 -0500

    CPP solution to Abbreviation

--- Abbreviation/CPP/Abbreviation.cpp
+++ Abbreviation/CPP/Abbreviation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,15 +8,44 @@ using namespace std;
 
 string getResult(string &a, string &b)
 {
-    int bStart = 0;
-    int bEnd = b.size() - 1;
+    vector<vector<bool>> dp;
+    int aSize = a.size();
+    int bSize = b.size();
 
-    int aStart = 0;
-    int aEnd = a.size() - 1;
-
-    while (bStart <= bEnd && aStart <= aEnd)
+    for (int i = 0; i <= bSize; i++)
     {
+        dp.push_back(vector<bool>(aSize));
     }
+
+    dp[0][0] = true;
+
+    for (int bi = 0; bi <= bSize; bi++)
+    {
+        for (int ai = 0; ai <= aSize; ai++)
+        {
+            if (bi == 0 && ai != 0)
+            {
+                dp[bi][ai] = islower(a[ai - 1]) && dp[bi][ai - 1];
+            }
+            else if (bi != 0 && ai != 0)
+            {
+                if (a[ai - 1] == b[bi - 1])
+                {
+                    dp[bi][ai] = dp[bi - 1][ai - 1];
+                }
+                else if (toupper(a[ai - 1]) == b[bi - 1])
+                {
+                    dp[bi][ai] = dp[bi - 1][ai - 1] || dp[bi][ai - 1];
+                }
+                else if (!isupper(a[ai - 1]))
+                {
+                    dp[bi][ai] = dp[bi][ai - 1];
+                }
+            }
+        }
+    }
+
+    return dp[bSize][aSize] ? "YES" : "NO";
 }
 
 int main()
@@ -31,7 +61,7 @@ int main()
         cin >> a;
         cin >> b;
 
-        cout << getResult(a, b);
+        cout << getResult(a, b) << "\n";
     }
 
     return 0;
