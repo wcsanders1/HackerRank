28954d99f7077fda59efc06bd30ddf7cd08e803c
William Sanders <william.sanders076@gmail.com>
2020-12-11 06:12:00 -0500

    Generate primes

--- PrimeXOR/CPP/PrimeXOR.cpp
+++ PrimeXOR/CPP/PrimeXOR.cpp
@@ -4,15 +4,62 @@
 
 using namespace std;
 
-#define mod 1000000007;
+const int mod = 1000000007;
+const int primeSize = 100000;
+
+bool primes[primeSize];
+
+void setPrimes()
+{
+    for (int i = 0; i < primeSize; i++)
+    {
+        primes[i] = true;
+    }
+
+    int prime = 2;
+
+    while (prime < primeSize)
+    {
+        int start = pow(prime, 2);
+
+        if (start >= primeSize)
+        {
+            return;
+        }
+
+        while (start < primeSize)
+        {
+            primes[start] = false;
+            start += prime;
+        }
+
+        for (int i = prime + 1; i < mod; i++)
+        {
+            if (primes[i])
+            {
+                prime = i;
+                break;
+            }
+        }
+    }
+}
 
 long getResult(vector<int> &nums)
 {
+    int result = 0;
+
+    for (int i : nums)
+    {
+        result ^= i;
+    }
+
     return 0;
 }
 
 int main()
 {
+    setPrimes();
+
     int q;
     cin >> q;
 
